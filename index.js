d1ab0d8877ab35e7f8b5ed7ab2d75534152d318c
Nguyen Phuong <nguyenphuong@Nguyens-MacBook-Pro.local>
2019-11-07 11:09:11 +0700

    module init

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { RnSmartConfigP } = NativeModules;
+
+export default RnSmartConfigP;
---
+++ ios/RnSmartConfigP.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RnSmartConfigP : NSObject <RCTBridgeModule>
+
+@end
